5b654105a1c24cb400923c5c01e5dfe4a80818fe
kimani-kevin <k2689399@gmail.com>
2026-05-25 20:59:39 +0300

    this, i dont know what number of commit i in. anyway i rewrote the entire code so that i can implement the push and pop commands b the help of the myarray[array].

--- Stack_and_queue.js
+++ Stack_and_queue.js
@@ -1,75 +1,41 @@
+const myarray=[];
+
 class stack{
-
-
-constructor(data, address) {
-    data=this.data;
-    address=this.address;
-}
-
-push(data, address) {
- address=address+1;
-
- let element={};
- element.data=data;
- element.address=address;
-return element;
-}
-
-
-traversal(address){
-  for(let i=address; i>0; i=i-1){
-    if(i==n){
-        console.log(i);
-        return address;
+    constructor(data){
+        data=this.data;
     }
-  }  
-}
-}
 
-
-
-let stack0=new stack();
-let stack1=new stack();
-let stack2=new stack();
-
-let x= stack0.push("Stake",0);
-let y=stack1.push("Chicken",1);
-let z=stack2.push("Fish",2);
-
-let myarray=[];
-myarray[1]=x.address;
-myarray[2]=y.address;
-myarray[3]=z.address;
-
-for(let i=0; i<myarray.length; i++){
-let n=2;
-    if (i==n || i>n){
-        if(y.address==i){
-            delete y.data;
-            delete y.address;
-            console.log(y);
-        }
-        if(z.address==i){
-            delete z.data;
-            delete z.address;
-            console.log(y);
-        }
-
+    push(data, myarray){
+        let i=myarray.length;
+        myarray[i]=data;
+        return console.log(myarray[i]+" has been pushed");
     }
-    console.log(y);
-    
+
+    pop(myarray){
+        let i=myarray.length;
+        if (i===0){
+            console.log("STACK UNDERFLOW")
+        }
+        myarray.pop();
+        return console.log(myarray.length);
+    }
 }
 
 
-console.log(x.address);
-console.log(y.address);
-console.log(z.address);
+let stack0= new stack();
 
 
-  
-//stack0.push("Itel");
-//stack0.push("Apple")
-//console.log(stack1.address);
-//console.log(address);
+stack0.push("Hello world", myarray);
+stack0.push("This is the second element", myarray);
+stack0.push("The stack is forming up!!!", myarray);
+stacko.push("This is the third element", myarray);
+stack0.push("This is the fourth element", myarray);
 
+
+console.log(myarray.length);
+
+stack0.pop(myarray);
+stack0.pop(myarray);
+stack0.pop(myarray);
+stack0.pop(myarray);
 
